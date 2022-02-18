4dba2918414b3814915b3d5b3ccabe08c2d79609
QuentinWidlocher <qwidlocher@gmail.com>
2022-02-18 21:59:28 +0100

    🚧  WIP - Route transitions

--- app/root.tsx
+++ app/root.tsx
@@ -1,28 +1,21 @@
-import {
-	Links,
-	LinksFunction,
-	LiveReload,
-	Meta,
-	Outlet,
-	Scripts,
-	ScrollRestoration,
-	useMatches,
-} from "remix";
-import type { MetaFunction } from "remix";
-import cssuiCore from "css-ui-lib/cssui.css";
-import cssuiTooltip from "css-ui-lib/tooltip/tooltip.css";
+import { Links, LinksFunction, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from "remix"
+import type { MetaFunction } from "remix"
+import cssuiCore from "css-ui-lib/cssui.css"
+import cssuiTooltip from "css-ui-lib/tooltip/tooltip.css"
+import animate from "animate.css"
 
 export const meta: MetaFunction = () => {
-	return { title: "Quentin Widlocher" };
-};
+	return { title: "Quentin Widlocher" }
+}
 
 export const links: LinksFunction = () => {
 	return [
 		{ rel: "stylesheet", href: cssuiCore },
 		{ rel: "stylesheet", href: cssuiTooltip },
 		{ rel: "stylesheet", href: "/tailwindcss" },
-	];
-};
+		{ rel: "stylesheet", href: animate },
+	]
+}
 
 export default function App() {
 	return (
@@ -35,25 +28,13 @@ export default function App() {
 				<meta name="twitter:card" content="summary_large_image" />
 				<meta name="twitter:site" content="@Lazard_" />
 				<meta name="twitter:title" content="Quentin Widlocher" />
-				<meta
-					name="twitter:description"
-					content="🇫🇷 French web developer, amateur game developer and a learning enthusiast !"
-				/>
-				<meta
-					name="twitter:image"
-					content="https://quentin.widlocher.com/assets/images/profil.webp"
-				/>
+				<meta name="twitter:description" content="🇫🇷 French web developer, amateur game developer and a learning enthusiast !" />
+				<meta name="twitter:image" content="https://quentin.widlocher.com/assets/images/profil.webp" />
 				<meta property="og:title" content="Quentin Widlocher" />
-				<meta
-					property="og:description"
-					content="🇫🇷 French web developer, amateur game developer and a learning enthusiast !"
-				/>
+				<meta property="og:description" content="🇫🇷 French web developer, amateur game developer and a learning enthusiast !" />
 				<meta property="og:url" content="https://quentin.widlocher.com" />
 				<meta property="og:type" content="website" />
-				<meta
-					property="og:image"
-					content="https://quentin.widlocher.com/assets/images/gradient-background.webp"
-				/>
+				<meta property="og:image" content="https://quentin.widlocher.com/assets/images/gradient-background.webp" />
 				<meta property="og:image:alt" content="Quentin Widlocher" />
 				<meta property="og:image:width" content="1280" />
 				<meta property="og:image:height" content="800" />
@@ -69,45 +50,22 @@ export default function App() {
 					}
 				</script>
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
-				<link
-					rel="preconnect"
-					href="https://fonts.gstatic.com"
-					crossOrigin="true"
-				/>
-				<link
-					href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;700&display=swap"
-					rel="stylesheet"
-				/>
-				<link
-					rel="apple-touch-icon"
-					sizes="180x180"
-					href="assets/icons/apple-touch-icon.png"
-				/>
-				<link
-					rel="icon"
-					type="image/png"
-					sizes="32x32"
-					href="assets/icons/favicon-32x32.png"
-				/>
-				<link
-					rel="icon"
-					type="image/png"
-					sizes="16x16"
-					href="assets/icons/favicon-16x16.png"
-				/>
+				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+				<link href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;700&display=swap" rel="stylesheet" />
+				<link rel="apple-touch-icon" sizes="180x180" href="assets/icons/apple-touch-icon.png" />
+				<link rel="icon" type="image/png" sizes="32x32" href="assets/icons/favicon-32x32.png" />
+				<link rel="icon" type="image/png" sizes="16x16" href="assets/icons/favicon-16x16.png" />
 				<link rel="manifest" href="/manifest.json" />
 			</head>
 			<body className="h-full">
 				<Outlet />
 				<ScrollRestoration />
 				<Scripts />
-				{process.env.NODE_ENV != "development" ? (
-					<script src="/sw_launcher.js" />
-				) : null}
+				{process.env.NODE_ENV != "development" ? <script src="/sw_launcher.js" /> : null}
 				{process.env.NODE_ENV === "development" && <LiveReload />}
 			</body>
 		</html>
-	);
+	)
 }
 export function CatchBoundary() {
 	return (
@@ -119,12 +77,10 @@ export function CatchBoundary() {
 			</head>
 			<body>
 				<div className="flex h-screen w-screen flex-col items-center justify-center bg-slate-800">
-					<h1 className="text-4xl text-white">
-						Oops, this is still WIP sorry 🚧
-					</h1>
+					<h1 className="text-4xl text-white">Oops, this is still WIP sorry 🚧</h1>
 				</div>
 				<Scripts />
 			</body>
 		</html>
-	);
+	)
 }
--- app/routes/__main.tsx
+++ app/routes/__main.tsx
@@ -1,16 +1,38 @@
-import { Outlet } from "remix";
-import NavBar from "~/components/navbar";
+import { Outlet, useLocation, useOutlet } from "remix"
+import NavBar from "~/components/navbar"
+import { SwitchTransition, CSSTransition, TransitionGroup } from "react-transition-group"
+import { useState } from "react"
+
+function StaticOutlet() {
+	let [element] = useState(useOutlet())
+	return element
+}
 
 export default function Index() {
-  return (
-    <>
-      <main className="grid h-full grid-cols-[auto_1fr] overflow-hidden text-white lg:grid-cols-[20rem_1fr]">
-        <NavBar />
-        <section tabIndex={-1} className="overflow-y-auto bg-gray-900/80">
-          <Outlet />
-        </section>
-      </main>
-      <div className="fixed top-0 -z-10 min-h-screen w-screen bg-blue-900 bg-[url('/assets/images/gradient-background.webp')] bg-cover bg-center backdrop-blur-3xl"></div>
-    </>
-  );
+	let location = useLocation()
+
+	return (
+		<>
+			<main className="grid h-full grid-cols-[auto_1fr] overflow-hidden text-white lg:grid-cols-[20rem_1fr]">
+				<NavBar />
+				<section tabIndex={-1} className="overflow-y-auto bg-gray-900/80">
+					<SwitchTransition mode="out-in">
+						<CSSTransition
+							key={location.pathname}
+							addEndListener={(node, done) => {
+								node.addEventListener("animationend", done, false)
+							}}
+							classNames={{ enterActive: "animate__fadeIn", exitActive: "animate__fadeOut" }}
+						>
+							<div className="animate__animated animate__faster">
+								<span>{location.pathname}</span>
+								<StaticOutlet />
+							</div>
+						</CSSTransition>
+					</SwitchTransition>
+				</section>
+			</main>
+			<div className="fixed top-0 -z-10 min-h-screen w-screen bg-blue-900 bg-[url('/assets/images/gradient-background.webp')] bg-cover bg-center backdrop-blur-3xl"></div>
+		</>
+	)
 }
